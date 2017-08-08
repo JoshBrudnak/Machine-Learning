065dc7dd3c3530c2482d9ad53c42b544c189a9c9
Josh31415 <jobrud314@gmail.com>
2017-08-07 23:38:17 -0400

    finished first training step

--- PredictGrades.py
+++ PredictGrades.py
@@ -13,7 +13,7 @@ class PredictGrades:
     
     return weights
 
-  def sigmoid(x)
+  def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
   trainingData = [] 
@@ -22,39 +22,62 @@ class PredictGrades:
   features = 3
   layerNodes = 4
   
-  for line in open("trainingData", "r"):
+  # parse json training data
+  for line in open("trainingData.json", "r"):
     trainingData.append(json.loads(line))
-  for i in range(0, layerNodes):
-    weight.append(getWeights(features))
-  if layers > 1:
-    for i in range(0, layers):
-      for j in range(0, layerNodes):
-        weight.append(getWeights(4))
-  for i in range(0, layerNodes):
-    weight.append(getWeights(1))
 
-  nodeResults = []
-  for j in range(0, layerNodes):
-    w = np.matrix(weight[j])
-    f = np.matrix(
-      trainingData[j]["timeStudied"], 
-      trainingData[j]["timeSlept"], 
-      trainingData[j]["classLevel"]
+  for t in range(0, len(trainingData)):
+    # get initial weights
+    for i in range(0, layerNodes):
+      weight.append(getWeights(features))
+
+    if layers > 1:
+      for i in range(0, layers):
+        for j in range(0, layerNodes):
+          weight.append(getWeights(4))
+    for i in range(0, layerNodes):
+      weight.append(getWeights(4))
+
+    nodeResults = []
+    for j in range(0, layerNodes):
+      w = np.matrix([weight[j][0], weight[j][1], weight[j][2]])
+      f = np.matrix([
+        [trainingData[t]["timeStudied"]],
+        [trainingData[t]["timeSlept"]], 
+        [trainingData[t]["classLevel"]]
+        ]
+      )
+      result = w * f
+      finValue = sigmoid(result)      
+      nodeResults.append(finValue.item(0))
+
+    if layers > 1:
+      for i in range(0, layers):
+        w = np.matrix([weight[j][0], weight[j][1], weight[j][2], weight[j][3]])
+        f = np.matrix(
+          [nodeResults[len(nodeResults) - 4]],
+          [nodeResults[len(nodeResults) - 3]],
+          [nodeResults[len(nodeResults) - 2]],
+          [nodeResults[len(nodeResults) - 1]]
+        )
+        result = w * f
+        finValue = sigmoid(result)
+        nodeResults.append(finValue.item(0))
+
+    index = len(weight) - 1
+    w = np.matrix([weight[index][0], weight[index][1], weight[index][2], weight[index][3]])
+    f = np.matrix([
+      [nodeResults[len(nodeResults) - 4]],
+      [nodeResults[len(nodeResults) - 3]],
+      [nodeResults[len(nodeResults) - 2]],
+      [nodeResults[len(nodeResults) - 1]]
+      ]
     )
     result = w * f
-    finValue = sigmoid(result)      
-    nodeResults.append(finValue)
+    finValue = sigmoid(result)
+    finalGrade = finValue.item(0)
 
-  if layers > 1:
-  for i in range(0, layers):
-    w = np.matrix(weight[j])
-    f = np.matrix(
-      trainingData[j]["timeStudied"], 
-      trainingData[j]["timeSlept"], 
-      trainingData[j]["classLevel"]
-    )
-    result = w * f
-    finValue = sigmoid(result)      
-    nodeResults.append(finValue)
-
-  print(weight)
+    print("Time Studied " + str(trainingData[t]["timeStudied"]))
+    print("Time Slept " + str(trainingData[t]["timeSlept"]))
+    print("Class Level " + str(trainingData[t]["classLevel"]))
+    print("Grade " + str(finalGrade))
